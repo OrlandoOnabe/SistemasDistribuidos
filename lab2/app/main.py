4a2082e052145228ffe7bc4ec45ce1a206576b82
Orlando N. Bertholdo <148660140+OrlandoOnabe@users.noreply.github.com>
2026-08-21 00:50:00 +0000

    atualizado

--- lab2/app/main.py
+++ lab2/app/main.py
@@ -1,7 +1,67 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "hello world"}
+
+counter = 0
+@app.get("/count")
+def get_count():
+    global counter
+    counter += 1
+    return counter
+
+@app.get("/hello")
+def hello_world():
+    return "Hello, world"
+
+@app.get("/hello/{name}")
+def hello(name):
+    return f"Hello, {name}"
+
+@app.get("/hello/")
+def hello(parameter = "World"):
+    return f"Hello, {parameter}"
+
+class Pessoa(BaseModel):
+    nome: str
+    sobrenome: str
+    idade: int
+
+@app.post("/pessoa/")
+def criar_pessoa(pessoa: Pessoa):
+    return pessoa
+
+tarefas = list()
+
+class Tarefa(BaseModel):
+    tarefa: str
+    prioridade: int
+    feito: bool
+
+@app.get("/")
+def root():
+    return tarefas
+
+@app.get("/tarefa/{pos}")
+def get_tarefa(pos: int):
+    return tarefas[pos]
+
+@app.post("/adicionar/")
+def criar_tarefa(tarefa: Tarefa):
+    tarefa.feito = False
+    tarefas.append(tarefa)
+    return len(tarefas)
+
+@app.put("/feito/{pos}")
+def marcar_feito(pos: int):
+    tarefas[pos].feito = True
+    return tarefas[pos]
+
+@app.delete("/deletar/{pos}")
+def deletar_tarefa(pos: int):
+    tarefa = tarefas.pop(pos)
+    return tarefa
