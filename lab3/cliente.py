c47d34883d5cdae7d37863be7b606551b47ad5d7
Orlando N. Bertholdo <148660140+OrlandoOnabe@users.noreply.github.com>
2026-08-28 23:25:48 +0000

    feito

--- lab3/cliente.py
+++ lab3/cliente.py
@@ -1,16 +1,64 @@
 import grpc
-import helloworld_pb2
-import helloworld_pb2_grpc
-from time import sleep
-
-print("Cliente conectando com servidor", flush=True)
+import task_manager_pb2
+import task_manager_pb2_grpc
 
 porta = "50051"
 endereco = "servidor"
 
+def connection_test(stub):
+    print("Testando conexão com servidor")
+    request = task_manager_pb2.ConnRequest(message="SYN")
+    response = stub.ConnectionTest(request)
+    print(f"Resposta: {response}", flush=True)
+
+def create_task(stub, title, description):
+    request = task_manager_pb2.CreateRequest(title=title, description=description)
+    response = stub.Create(request)
+    print(f"Tarefa criada: {response}", flush=True)
+    return response.id
+
+def get_task(stub, id):
+    request = task_manager_pb2.GetRequest(id=str(id))
+    try:
+        response = stub.Get(request)
+        print(f"Tarefa: {response}", flush=True)
+    except grpc.RpcError as e:
+        print(e.details(), flush=True)
+
+def list_tasks(stub):
+    request = task_manager_pb2.ListRequest()
+    response = stub.List(request)
+    print("Lista de tarefas:", flush=True)
+    for task in response.tasks:
+        print(task, flush=True)
+
+def update_task(stub, id, title, description, status):
+    request = task_manager_pb2.UpdateRequest(id=str(id), title=title, description=description, status=status)
+    try:
+        response = stub.Update(request)
+        print(f"Tarefa atualizada: {response}", flush=True)
+    except grpc.RpcError as e:
+        print(e.details(), flush=True)
+
+def delete_task(stub, id):
+    request = task_manager_pb2.DeleteRequest(id=str(id))
+    try:
+        stub.Delete(request)
+        print("Tarefa apagada", flush=True)
+    except grpc.RpcError as e:
+        print(e.details(), flush=True)
+
+print("Cliente conectando com o servidor",flush=True)
+porta = "50051"
+endereco = "servidor"
 with grpc.insecure_channel(f"{endereco}:{porta}") as channel:
-    stub = helloworld_pb2_grpc.GreeterStub(channel)
-    while True:
-        resposta = stub.HelloWorld(helloworld_pb2.MsgCliente(mensagem="hello"))
-        print(f"Resposta do servidor: {resposta.mensagem}", flush=True)
-        sleep(0.5)
+    stub = task_manager_pb2_grpc.TaskManagerStub(channel)
+    connection_test(stub)
+
+    create_task(stub, "teste", "teste de tarefa")
+    get_task(stub, 1)
+    list_tasks(stub)
+    update_task(stub, 1, "atualizacao", "teste de update", "completa")
+    list_tasks(stub)
+    delete_task(stub, 1)
+    list_tasks(stub)
--- lab3/servidor.py
+++ lab3/servidor.py
@@ -1,16 +1,56 @@
 from concurrent import futures
 import grpc
-import helloworld_pb2
-import helloworld_pb2_grpc
+import task_manager_pb2
+import task_manager_pb2_grpc
 
-class Greeter(helloworld_pb2_grpc.GreeterServicer):
-    def HelloWorld(self, request, context):
-        print(f"Mensagem do cliente: {request.mensagem}",flush=True)
-        return helloworld_pb2.MsgServidor(mensagem="World")
+class TaskManagerService(task_manager_pb2_grpc.TaskManagerServicer):
+    def __init__(self):
+        self.tasks = {}
+        self.counter = 0
+
+    def ConnectionTest(self, request, context):
+        print(f"Mensagem do cliente: {request.message}", flush=True)
+        return task_manager_pb2.ConnReply(message="ACK")
+
+    def Create(self, request, context):
+        self.counter += 1
+        id = str(self.counter)
+        task = task_manager_pb2.Task(id=id, title=request.title, description=request.description, status="não completo")
+        self.tasks[id] = task
+        return task
+
+    def Get(self, request, context):
+        id = request.id
+        if id in self.tasks:
+            return self.tasks[id]
+        else:
+            context.abort(grpc.StatusCode.NOT_FOUND, "ERRO: tarefa não encontrada")
+
+    def List(self, request, context):
+        return task_manager_pb2.ListResponse(tasks=self.tasks.values())
+
+    def Update(self, request, context):
+        id = request.id
+        if id in self.tasks:
+            task = self.tasks[id]
+            task.title = request.title
+            task.description = request.description
+            task.status = request.status
+            return task
+        else:
+            context.abort(grpc.StatusCode.NOT_FOUND, "ERRO: tarefa não encontrada")
+
+    def Delete(self, request, context):
+        id = request.id
+        if id in self.tasks:
+            del self.tasks[id]
+            return task_manager_pb2.Empty()
+        else:
+            context.abort(grpc.StatusCode.NOT_FOUND, "ERRO: tarefa não encontrada")
 
 endereco = "[::]:50051"
 servidor = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
-helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), servidor)
+task_manager_pb2_grpc.add_TaskManagerServicer_to_server(TaskManagerService(), servidor)
 
 servidor.add_insecure_port(endereco)
 servidor.start()
